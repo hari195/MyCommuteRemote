f8806b3377187912808fdaf709bc74a7e5be5c8a
Harikrishnan R <harikrishnanr195@gmail.com>
2017-02-25 12:46:05 +0530

    Works without hardware connected

--- BusApp.py
+++ BusApp.py
@@ -8,8 +8,8 @@ from getpass import getpass
 import PIL
 import serial
 
-ser=serial.Serial('/dev/ttyACM1',9600)
-ser.write('T')
+#ser=serial.Serial('/dev/ttyACM1',9600)
+#ser.write('T')
 
 
 
@@ -28,34 +28,34 @@ previous_data = "NULL2"
 
 
 while(True):
-    
+
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)	
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     pil=PIL.Image.fromarray(gray)
     width,height = pil.size
     raw=pil.tostring()
     img=zbar.Image(width,height,'Y800',raw)
-    
-    
+
+
     cv2.imshow('frame',gray)
     scanner.scan(img)
     for symbol in img:
         if str(symbol.type) == "QRCODE":
-		
-		if str(symbol.data) !=  previous_data:  
+
+		if str(symbol.data) !=  previous_data:
 		   #Send POST request with data as payload and token from login
 		   #Check response for validity. If valid allow passenger and display pop up message
 		   #If not valid, sound alarm
 		   #os.system("/usr/bin/canberra-gtk-play --id='system-ready'")
    		   #os.system("/usr/bin/canberra-gtk-play --id='alarm-clock-elapsed'")
-	
+
 		   print symbol.data
-		   os.system("notify-send -t 900 \"WELCOME!!\" \"NAME\"")  
+		   os.system("notify-send -t 900 \"WELCOME!!\" \"NAME\"")
         previous_data = symbol.data
     del(img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
 
 
 cap.release()
