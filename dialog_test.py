3f638ad8efebdf16135affd3e313cfc0964245fe
Harikrishnan R <harikrishnanr195@gmail.com>
2017-02-26 00:05:36 +0530

    Driver login dialog done

--- dialog_test.py
+++ dialog_test.py
@@ -2,25 +2,28 @@ from Tkinter import *
 
 def callback():
     print("hi")
-    print E1
-    
+    print E1.get()
+    print E2.get()
+    print E3.get()
     top.destroy()
-
+v="dfw"
 top = Tk()
 L1 = Label(top, text="User Name")
 L1.grid(row=0, column=0)
-E1 = Entry(top, bd = 5)
+E1 = Entry(top, bd = 5, textvariable=v)
 E1.grid(row=0, column=1)
+print E1.get()
+
 
 L1 = Label(top, text="Password")
 L1.grid(row=1, column=0)
-E1 = Entry(top, bd = 5, show='*')
-E1.grid(row=1, column=1)
+E2 = Entry(top, bd = 5, show='*')
+E2.grid(row=1, column=1)
 
 L1 = Label(top, text="Bus Route")
 L1.grid(row=2, column=0)
-E1 = Entry(top, bd = 5)
-E1.grid(row=2, column=1)
+E3 = Entry(top, bd = 5)
+E3.grid(row=2, column=1)
 
 
 MyButton1 = Button(top, text="Submit", width=10, command=callback)
